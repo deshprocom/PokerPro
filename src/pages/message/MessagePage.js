453020201a1d16418385a9c993b5d0e38b67e45a
lorne <2270333671@qq.com>
2017-05-24 14:48:33 +0800

    消息

--- src/pages/message/MessagePage.js
+++ src/pages/message/MessagePage.js
@@ -11,8 +11,11 @@ import {Colors, Fonts, Images, ApplicationStyles} from '../../Themes';
 import I18n from 'react-native-i18n';
 import {NavigationBar, UltimateListView, ImageLoad} from '../../components';
 import {NoDataView, LoadErrorView, LoadingView} from '../../components/load';
+import {GET_NOTIFICATIONS} from '../../actions/ActionTypes';
+import {fetchNotifications} from '../../actions/AccountAction';
 
-export default class MessagePage extends Component {
+
+class MessagePage extends Component {
 
     constructor(props) {
         super(props);
@@ -21,6 +24,7 @@ export default class MessagePage extends Component {
         this.state = {
             listTicket: dataList,
         };
+        this.listPage = 1;
 
     }
 
@@ -34,7 +38,63 @@ export default class MessagePage extends Component {
                 leftImageStyle={{height:19,width:11,marginLeft:20,marginRight:20}}
                 leftBtnPress={()=>router.pop()}/>
 
-            <View style={styles.listItem}>
+            <UltimateListView
+                ref={(ref) => this.listView = ref}
+                keyExtractor={(item, index) => `${item.race_id}`}
+                onFetch={this.onFetch}
+                refreshableMode="advanced"
+                separator={()=>  <View style={{height:10}}/>}
+                rowView={this._itemListView}
+                refreshableTitlePull={I18n.t('pull_refresh')}
+                refreshableTitleRelease={I18n.t('release_refresh')}
+                dateTitle={I18n.t('last_refresh')}
+                allLoadedText={I18n.t('no_more')}
+                waitingSpinnerText={I18n.t('loading')}
+                emptyView={()=>{
+             return <NoDataView/>;
+             }}
+            />
+
+        </View>)
+    }
+
+    componentWillReceiveProps(newProps) {
+        const {actionType, notices, loading, hasData} = newProps;
+        if (actionType === GET_NOTIFICATIONS
+            && loading !== this.props.loading
+            && hasData) {
+
+            const {notifications} = notices;
+            if (this.listPage == 1) {
+                this.listView.postRefresh(notifications, 1);
+            }
+        }
+
+    }
+
+    onFetch = async(page = 1, startFetch, abortFetch) => {
+        try {
+            this.listPage = page;
+
+            if (page === 1) {
+                this._onRefresh();
+            }
+
+        } catch (err) {
+            abortFetch();
+            console.log(err);
+        }
+    };
+
+    _onRefresh = () => {
+        this.props.getNotices()
+    };
+
+    _itemListView = (item, index) => {
+
+        const {notify_type} = item;
+        if (notify_type === 'order') {
+            return (  <View style={styles.listItem}>
                 <View style={styles.itemTitle}>
                     <Text style={styles.txtPay}>付款成功</Text>
                     <View style={{flex:1}}/>
@@ -57,9 +117,9 @@ export default class MessagePage extends Component {
 
                 <View style={{height:15}}/>
 
-            </View>
-            <View style={{height:10}}/>
-            <View style={styles.listItem}>
+            </View>)
+        } else if (notify_type === 'certification') {
+            return ( <View style={styles.listItem}>
                 <View style={styles.itemTitle}>
                     <Text style={[styles.txtPay,styles.txtRed]}>实名认证失败</Text>
                     <View style={{flex:1}}/>
@@ -70,95 +130,97 @@ export default class MessagePage extends Component {
 
                 <Text style={styles.txtSource}>来自：Poker Pro官方客服</Text>
 
-            </View>
-
-            {/* <UltimateListView
-             ref={(ref) => this.listView = ref}
-             keyExtractor={(item, index) => `${item.race_id}`}
-             onFetch={this.onFetch}
-             refreshableMode="advanced"
-             rowView={this._itemListView}
-             refreshableTitlePull={I18n.t('pull_refresh')}
-             refreshableTitleRelease={I18n.t('release_refresh')}
-             dateTitle={I18n.t('last_refresh')}
-             allLoadedText={I18n.t('no_more')}
-             waitingSpinnerText={I18n.t('loading')}
-             emptyView={()=>{
-             return <NoDataView/>;
-             }}
-             />*/}
-
-        </View>)
-    }
-
-    onFetch = async(page = 1, startFetch, abortFetch) => {
-
-    };
-
-    _itemListView = (item, index) => {
-
+            </View>)
+        }
     }
 
 }
 
+const
+    bindAction = dispatch => ({
+        getNotices: () => dispatch(fetchNotifications())
+    })
+    ;
 
-const styles = StyleSheet.create({
-    itemTitle: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: 40
-    },
-    txtPay: {
-        fontSize: 18,
-        color: '#34BA3C'
-    },
-    txtTime: {
-        fontSize: 12,
-        color: Colors._AAA
-    },
-    imgRace: {
-        height: 84,
-        width: 65
-    },
-    itemView: {
-        height: 84,
-        flexDirection: 'row',
-    },
-    txtContent: {
-        fontSize: 14,
-        color: Colors.txt_666,
-        marginTop: 10,
-        lineHeight:20
-    },
-    txtNum: {
-        fontSize: 12,
-        color: Colors._AAA
-    },
-    itemContent: {
-        flex: 1,
-        marginLeft: 10
-    },
-    listItem: {
-        paddingLeft: 18,
-        backgroundColor: 'white',
-        paddingRight:18
-    },
-    txtRed: {
-        color: '#F34A4A'
-    },
-    txtNotice: {
-        fontSize: 14,
-        color: Colors.txt_666,
-        marginTop:10,
-        lineHeight:20
-    },
-    txtSource: {
-        fontSize: 12,
-        color: Colors._888,
-        alignSelf: 'flex-end',
-        marginTop:15,
-        marginBottom:17
-    }
+const
+    mapStateToProps = state => ({
+        loading: state.AccountState.loading,
+        error: state.AccountState.error,
+        hasData: state.AccountState.hasData,
+        actionType: state.AccountState.actionType,
+        notices: state.AccountState.notices
+
+    });
+
+export
+default
+
+connect(mapStateToProps, bindAction)
+
+(
+    MessagePage
+)
+;
 
 
-})
+const
+    styles = StyleSheet.create({
+        itemTitle: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            height: 40
+        },
+        txtPay: {
+            fontSize: 18,
+            color: '#34BA3C'
+        },
+        txtTime: {
+            fontSize: 12,
+            color: Colors._AAA
+        },
+        imgRace: {
+            height: 84,
+            width: 65
+        },
+        itemView: {
+            height: 84,
+            flexDirection: 'row',
+        },
+        txtContent: {
+            fontSize: 14,
+            color: Colors.txt_666,
+            marginTop: 10,
+            lineHeight: 20
+        },
+        txtNum: {
+            fontSize: 12,
+            color: Colors._AAA
+        },
+        itemContent: {
+            flex: 1,
+            marginLeft: 10
+        },
+        listItem: {
+            paddingLeft: 18,
+            backgroundColor: 'white',
+            paddingRight: 18
+        },
+        txtRed: {
+            color: '#F34A4A'
+        },
+        txtNotice: {
+            fontSize: 14,
+            color: Colors.txt_666,
+            marginTop: 10,
+            lineHeight: 20
+        },
+        txtSource: {
+            fontSize: 12,
+            color: Colors._888,
+            alignSelf: 'flex-end',
+            marginTop: 15,
+            marginBottom: 17
+        }
+
+
+    })
