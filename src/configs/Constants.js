92025dba63b21f6142626521cac12e283a8323e8
lorne <2270333671@qq.com>
2017-06-23 17:55:51 +0800

    qq_share_key

--- src/configs/Constants.js
+++ src/configs/Constants.js
@@ -8,3 +8,7 @@ export const UMENG_ANDROID = '58da17cc1061d26493001174';//dev
 
 export const UMENG_IOS_PRODUCTION = '58da166b07fe657e860008f1';//ios_pokerpro
 export const UMENG_ANDROID_PRODUCTION = '58da162c9f06fd7065000052';//android_poperpro
+
+
+export const QQ_SHARE_ID = '1106155543';
+export const QQ_SHARE_KEY = 'kaI7hok5UcxwVaHv';
