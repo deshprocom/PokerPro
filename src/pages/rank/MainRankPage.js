9246265ba44e2b5b9c0fa9e23f07a9e61cda74c8
tony <2543459951@qq.com>
2017-07-13 18:39:56 +0800

    tony

--- src/pages/rank/MainRankPage.js
+++ src/pages/rank/MainRankPage.js
@@ -1,11 +1,13 @@
 
 import React,{Component} from 'react';
 import {View, Text} from 'react-native';
+import {RankList} from './RankList';
 
 export default class MainRankPage extends Component {
     render(){
         return(<View>
             <Text>排行</Text>
+            <RankList/>
         </View>)
     }
 }
--- src/pages/rank/RankList.js
+++ src/pages/rank/RankList.js
@@ -1,5 +1,10 @@
 import React,{Component} from 'react';
+import {View,Text} from 'react-native';
 
 export default class RankList extends Component {
-
+    render(){
+        return(<View>
+            <Text>sdgg</Text>
+        </View>)
+    }
 }
