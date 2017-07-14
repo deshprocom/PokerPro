ef48c8bc5a4c54a8a36bbbb7528a5c8ccaa0ac2a
lorne <2270333671@qq.com>
2017-07-14 11:26:38 +0800

    设置中英文翻译

--- src/pages/setting/SettingPage.js
+++ src/pages/setting/SettingPage.js
@@ -20,9 +20,6 @@ import StorageKey from '../../configs/StorageKey';
 
 import {setLocalLanguage} from '../../services/ConfigDao';
 
-const CANCEL_INDEX = 0;
-const options = [I18n.t('cancel'), '标准', '中', '大'];
-
 
 class SettingPage extends Component {
     state = {
@@ -107,7 +104,7 @@ class SettingPage extends Component {
                     onPress={() => {
                         this.ActionSheet.show();
                     }}
-                    name={'字体设置'}/>
+                    name={I18n.t('font_set')}/>
 
                 <View
                     style={{height: 1, marginLeft: 17, backgroundColor: Colors.bg_black}}/>
@@ -136,9 +133,9 @@ class SettingPage extends Component {
 
             <ActionSheet
                 ref={o => this.ActionSheet = o}
-                title={'字体选择'}
-                options={options}
-                cancelButtonIndex={CANCEL_INDEX}
+                title={I18n.t('font_select')}
+                options={[I18n.t('cancel'), I18n.t('font_stan'), I18n.t('font_mid'), I18n.t('font_big')]}
+                cancelButtonIndex={0}
                 destructiveButtonIndex={DESTRUCTIVE_INDEX}
                 onPress={this.handlePress}
             />
