78bccbf332a4a6bb701f4647dc82e800896dfae9
lorne <2270333671@qq.com>
2017-08-04 10:08:26 +0800

    排行年份

--- src/pages/rank/RankCheck.js
+++ src/pages/rank/RankCheck.js
@@ -1,4 +1,4 @@
-import React,{Component, PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import I18n from 'react-native-i18n';
 
@@ -12,7 +12,7 @@ class RankCheck extends Component {
         checkList: PropTypes.func
     };
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             check: false,
@@ -22,81 +22,81 @@ class RankCheck extends Component {
         }
     }
 
-    componentWillMount(checkData){
-        let length= this.props.checkData.length;
+    componentWillMount(checkData) {
+        let length = this.props.checkData.length;
         let arr = [];
-        for(let i = 0 ;i < length;i++){
+        for (let i = 0; i < length; i++) {
             let data = this.props.checkData[i];
-            let item = {index:i,infoData: data,select:false, info: ''};
+            let item = {index: i, infoData: data, select: false, info: ''};
             arr.push(item);
         }
 
         this.setState({
-            itemArr:arr,
+            itemArr: arr,
         });
     }
 
     selectedBtn = (key) => {
         const {allSelect} = this.state;
-        if(allSelect){
+        if (allSelect) {
+            this.allSelected();
+        }
+        let arr = this.state.itemArr;
 
-        }else{
-            let arr = this.state.itemArr;
+        if (arr[key].index == key) {
+            arr[key].select = !arr[key].select;
+            this.setState({
+                itemArr: arr
+            });
+        }
 
-            if(arr[key].index == key){
-                arr[key].select = !arr[key].select;
-                this.setState({
-                    itemArr: arr
-                });
+        let selectArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].select) {
+                selectArr.push(i)
             }
-
-            let selectArr = [];
-            for(let i=0;i<arr.length;i++){
-                if(arr[i].select){
-                    selectArr.push(i)
-                }
-            }
-            let selectLength = selectArr.length;
-            let selectNum = selectArr[1] - selectArr[0];
-            if(selectLength==2 && selectNum>1){
-                let start = selectArr[0] +1;
-                let end = selectArr[1];
-                for(let i=start;i<end;i++){
-                    arr[i].select = !arr[i].select;
-                    this.setState({
-                        itemArr: arr
-                    })
-                }
-            }
-
-            if(selectLength>2){
-                for(let i=0;i<selectLength;i++){
-                    if(selectArr[i] == key){
-                        // arr[key].select = true;
-                    }else{
-                        arr[selectArr[i]].select = false;
-                    }
-                }
-                arr[key].select = true;
+        }
+        let selectLength = selectArr.length;
+        let selectNum = selectArr[1] - selectArr[0];
+        if (selectLength == 2 && selectNum > 1) {
+            let start = selectArr[0] + 1;
+            let end = selectArr[1];
+            for (let i = start; i < end; i++) {
+                arr[i].select = !arr[i].select;
                 this.setState({
                     itemArr: arr
                 })
             }
         }
 
+        if (selectLength > 2) {
+            for (let i = 0; i < selectLength; i++) {
+                if (selectArr[i] == key) {
+                    // arr[key].select = true;
+                } else {
+                    arr[selectArr[i]].select = false;
+                }
+            }
+            arr[key].select = true;
+            this.setState({
+                itemArr: arr
+            })
+        }
+
+
     };
 
     cancelBtn = () => {
         let arr = this.state.itemArr;
         let arrClear = [];
-        for(let i = 0 ;i < arr.length;i++){
+        for (let i = 0; i < arr.length; i++) {
             let infoData = this.props.checkData[i];
-            let item = {index:i,infoData: infoData,select:false,info: ''};
+            let item = {index: i, infoData: infoData, select: false, info: ''};
             arrClear.push(item);
         }
 
         this.setState({
-            itemArr:arrClear,
+            itemArr: arrClear,
             allSelect: true
         });
     };
@@ -104,11 +104,12 @@ class RankCheck extends Component {
     sureBtn = () => {
         let arr = this.state.itemArr;
         let newArr = [];
-        for(let i=0;i<arr.length;i++){
-            if(arr[i].info){
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].info) {
                 newArr.push(arr[i].info);
             }
-        };
+        }
+        ;
         return newArr;
     };
 
@@ -118,7 +119,7 @@ class RankCheck extends Component {
             allSelect: !allSelect
         });
         let arr = this.state.itemArr;
-        for(let i=0;i<arr.length;i++){
+        for (let i = 0; i < arr.length; i++) {
             arr[i].select = false
         }
         this.setState({
@@ -127,21 +128,40 @@ class RankCheck extends Component {
     };
 
     lineView = (checkData) => {
-        if(this.props.checkData.length == 0){
-            return(
-                <View style={{height:1,backgroundColor: Colors.bg_f5,marginTop: 5,marginBottom: 20,width: 0}}></View>
+        if (this.props.checkData.length == 0) {
+            return (
+                <View
+                    style={{height: 1, backgroundColor: Colors.bg_f5, marginTop: 5, marginBottom: 20, width: 0}}></View>
             )
-        }else if(this.props.checkData.length == 1){
-            return(
-                <View style={{height:1,backgroundColor: Colors.bg_f5,marginTop: 5,marginBottom: 20,width: 72}}></View>
+        } else if (this.props.checkData.length == 1) {
+            return (
+                <View style={{
+                    height: 1,
+                    backgroundColor: Colors.bg_f5,
+                    marginTop: 5,
+                    marginBottom: 20,
+                    width: 72
+                }}></View>
             )
-        }else if(this.props.checkData.length == 2){
-            return(
-                <View style={{height:1,backgroundColor: Colors.bg_f5,marginTop: 5,marginBottom: 20,width: 155}}></View>
+        } else if (this.props.checkData.length == 2) {
+            return (
+                <View style={{
+                    height: 1,
+                    backgroundColor: Colors.bg_f5,
+                    marginTop: 5,
+                    marginBottom: 20,
+                    width: 155
+                }}></View>
             )
-        }else{
-            return(
-                <View style={{height:1,backgroundColor: Colors.bg_f5,marginTop: 5,marginBottom: 20,width: 239}}></View>
+        } else {
+            return (
+                <View style={{
+                    height: 1,
+                    backgroundColor: Colors.bg_f5,
+                    marginTop: 5,
+                    marginBottom: 20,
+                    width: 239
+                }}></View>
             )
         }
     };
@@ -149,36 +169,50 @@ class RankCheck extends Component {
     choiceBtn = (checkData) => {
         let arr = this.state.itemArr;
         const {allSelect} = this.state;
-        return(<View style={{flexDirection: 'row', flexWrap: 'wrap',justifyContent: 'flex-start'}}>
+        return (<View style={{flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start'}}>
             <TouchableOpacity onPress={() => {
-                            this.allSelected()
-                        }}
+                this.allSelected()
+            }}
                               style={styles.btn_style}>
-                <Image source={allSelect?Images.Group:Images.Group_em}
-                       style={{width: 72,height: 30,alignItems: 'center',justifyContent: 'center', backgroundColor:'transparent'}}>
-                    <Text style={{color: Colors._333,fontSize: 15}}>{I18n.t('all')}</Text>
+                <Image source={allSelect ? Images.Group : Images.Group_em}
+                       style={{
+                           width: 72,
+                           height: 30,
+                           alignItems: 'center',
+                           justifyContent: 'center',
+                           backgroundColor: 'transparent'
+                       }}>
+                    <Text style={{color: Colors._333, fontSize: 15}}>{I18n.t('all')}</Text>
                 </Image>
             </TouchableOpacity>
             {
-                this.props.checkData.map((item,key) => {
+                this.props.checkData.map((item, key) => {
                     let is_select = null;
-                    if(arr[key].index == key){
+                    if (arr[key].index == key) {
                         is_select = arr[key].select;
                     }
-                    if(arr[key].select){
+                    if (arr[key].select) {
                         arr[key].info = arr[key].infoData;
-                    }else{
+                    } else {
                         arr[key].info = '';
                     }
 
-                    return(
+                    return (
                         <TouchableOpacity key={key} onPress={() => {
-                            {this.selectedBtn(key);}
+                            {
+                                this.selectedBtn(key);
+                            }
                         }}
-                                  style={styles.btn_style}>
-                            <Image source={is_select?Images.Group:Images.Group_em}
-                                   style={{width: 72,height: 30,alignItems: 'center',justifyContent: 'center',backgroundColor: 'transparent'}}>
-                                <Text style={{color: Colors._333,fontSize: 15}}>{item}</Text>
+                                          style={styles.btn_style}>
+                            <Image source={is_select ? Images.Group : Images.Group_em}
+                                   style={{
+                                       width: 72,
+                                       height: 30,
+                                       alignItems: 'center',
+                                       justifyContent: 'center',
+                                       backgroundColor: 'transparent'
+                                   }}>
+                                <Text style={{color: Colors._333, fontSize: 15}}>{item}</Text>
                             </Image>
                         </TouchableOpacity>
                     )
@@ -188,9 +222,10 @@ class RankCheck extends Component {
 
     };
 
-    render(){
-        return(<View>
-            <Text style={{lineHeight: 21,fontSize: 15,color: Colors._888}} onPress={() => this.sureBtn()}>{this.props.checkTitle}</Text>
+    render() {
+        return (<View>
+            <Text style={{lineHeight: 21, fontSize: 15, color: Colors._888}}
+                  onPress={() => this.sureBtn()}>{this.props.checkTitle}</Text>
             {this.lineView()}
             {this.choiceBtn()}
         </View>)
