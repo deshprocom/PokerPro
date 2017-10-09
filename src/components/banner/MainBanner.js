f7e1a4543ea7f03ddba2a3270a0497d62f9bea37
lorne <2270333671@qq.com>
2017-10-09 16:51:31 +0800

    轮播图控件

---
+++ src/components/banner/MainBanner.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import Swiper from 'react-native-swiper';
+
+
+export default class MainBanner extends Component {
+
+
+    state = {
+        items: ['red', 'green', 'yellow', 'pink']
+    };
+
+    render() {
+
+        return (
+            <View style={{height: 200}}>
+                <Swiper
+                    autoplay>
+                    {this.state.items.map((item, key) => {
+                        return <View
+                            key={key}
+                            style={{backgroundColor: item, flex: 1}}>
+
+                        </View>
+                    })}
+
+                </Swiper>
+            </View>
+        )
+    }
+}
