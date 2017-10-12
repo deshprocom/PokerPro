4310a44315fb476f7efacae6b3828eeac9632d6f
lorne <2270333671@qq.com>
2017-10-12 09:50:11 +0800

    首页优化

--- src/pages/navigation/MainBanner.js
+++ src/pages/navigation/MainBanner.js
@@ -7,28 +7,9 @@ import {
 } from 'react-native';
 
 import Swiper from 'react-native-swiper';
-import {getMainBanners} from '../../services/NewsDao';
-import I18n from 'react-native-i18n';
 
 
 export default class MainBanner extends Component {
-
-
-    state = {
-        items: []
-    };
-
-    componentDidMount() {
-        getMainBanners(data => {
-
-
-            this.setState({
-                items: data.banners
-            });
-            console.log('items', this.state.items)
-        }, err => {
-        })
-    }
 
     render() {
 
@@ -37,8 +18,8 @@ export default class MainBanner extends Component {
                 <Swiper
                     autoplayTimeout={3}
                     autoplay>
-                    {this.state.items.map((item, key) => {
-                        return <Image  key={key} style={{height:200,width:'100%'}} source={{uri:item.image}}/>
+                    {this.props.banners.map((item, key) => {
+                        return <Image key={key} style={{height: 200, width: '100%'}} source={{uri: item.image}}/>
                     })}
 
                 </Swiper>
