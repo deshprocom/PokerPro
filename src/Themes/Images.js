3860792de1540fb686be8dd9ad5e197a13fde673
Millie <millie@deshpro.com>
2017-11-10 15:47:12 +0800

    完成订单页面

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -243,7 +243,8 @@ const images = {
     radio: require('../../source/cart/radio.png'),
     emptyCart: require('../../source/cart/emptyCart.png'),
     poker_key: require('../../source/navigation/poker_key.png'),
-    expand: require('../../source/order/expand.png')
+    expand: require('../../source/order/expand.png'),
+    positioning: require('../../source/order/positioning.png')
 
 
 
