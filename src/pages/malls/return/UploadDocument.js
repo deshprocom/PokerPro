59c024406c50ca5e098f5cbc6f36c5ff2ad7d93d
Millie <millie@deshpro.com>
2017-11-30 17:26:34 +0800

    退换货上传凭证

--- src/pages/malls/return/UploadDocument.js
+++ src/pages/malls/return/UploadDocument.js
@@ -6,34 +6,42 @@ import {NavigationBar, ActionSheet, ImagePicker} from '../../../components';
 import {strNotNull, isEmptyObject} from '../../../utils/ComonHelper';
 
 const picker = {
-    width: 100,
-    height: 100,
-    compressImageMaxWidth: 800,
-    compressImageMaxHeight: 800,
     compressImageQuality: 0.5,
-    cropperCircleOverlay: true,
-    cropping: false,
     multiple: true
 };
 
 export default class UploadDocument extends PureComponent {
-    state={
-        localImg: {},
-        showImg: ''
+    state = {
+        localImg: [1, 2, 3],
+        spliceIndex: 0
     };
 
     handlePress = (i) => {
         switch (i) {
             case 1:
                 ImagePicker.openCamera(picker).then(localImg => {
-                    this.setState({localImg, showImg: localImg.path})
+
+                    this.setState({
+                        localImg
+                    })
                 }).catch(e => {
                     alert(e.message ? e.message : e);
                 });
                 break;
             case 2: {
-                ImagePicker.openPicker(picker).then(localImg => {
-                    this.setState({localImg, showImg: localImg.path})
+                ImagePicker.openPicker(picker).then(images => {
+
+                    if (images.length <= 0) {
+                        return
+                    }
+                    const {localImg, spliceIndex} = this.state;
+                    let imgs = [...localImg]
+                    imgs.splice(spliceIndex, images.length, ...images);
+
+                    this.setState({
+                        localImg: imgs,
+                        spliceIndex: images.length
+                    })
                 }).catch(e => {
                     alert(e.message ? e.message : e);
                 });
@@ -42,18 +50,32 @@ export default class UploadDocument extends PureComponent {
     };
 
 
-    render(){
-        return(
+    render() {
+        return (
             <View style={styles.page}>
                 <Text style={styles.amountTxt}>{I18n.t('upload_document')}：</Text>
                 <View style={styles.uploadImg}>
-                    <TouchableOpacity
-                        onPress={() => {
+                    {this.state.localImg.map((item, index) => {
+                        return (
+                            <TouchableOpacity
+                                key={`mall_image${index}`}
+                                onPress={() => {
                             this.ActionSheet.show();
-                        }}
-                        style={styles.btnSelectImg}>
 
-                    </TouchableOpacity>
+                        }}
+                                style={styles.btnSelectImg2}>
+
+
+
+                                {item.path && <Image
+                                    source={{uri:item.path}}
+                                    style={{  height: 100,width: 100}}/>}
+
+
+                            </TouchableOpacity>
+                        )
+                    })}
+
                 </View>
 
                 <ActionSheet
@@ -65,36 +87,52 @@ export default class UploadDocument extends PureComponent {
                     onPress={this.handlePress}
                 />
             </View>
-        )}
+        )
+    }
 }
 const styles = StyleSheet.create({
-    page:{
-        height:165,
-        backgroundColor:'#FFFFFF',
-        marginTop:10,
-        flexDirection:'column',
-        alignItems:'center',
-        paddingBottom:20
+    page: {
+        height: 165,
+        backgroundColor: '#FFFFFF',
+        marginTop: 10,
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingBottom: 20
     },
-    amountTxt:{
+    amountTxt: {
         fontSize: 14,
-        color:'#333333',
-        marginLeft:17,
-        marginTop:14
+        color: '#333333',
+        marginLeft: 17,
+        marginTop: 14
     },
-    uploadImg:{
-        marginLeft:17,
-        flexDirection:'row',
-        alignItems:"center",
-        marginTop:14
+    uploadImg: {
+        marginLeft: 17,
+        marginRight: 17,
+        flexDirection: 'row',
+        alignItems: "center",
+        marginTop: 14,
+
     },
     btnSelectImg: {
         alignSelf: 'center',
-        height: 128,
-        width: 206,
+        height: 100,
+        width: 100,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#BBBBBB'
+        borderWidth: 1,
+        borderColor: '#CCCCCC',
+        borderStyle: 'dashed'
+    },
+    btnSelectImg2: {
+        alignSelf: 'center',
+        height: 100,
+        width: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#CCCCCC',
+        borderStyle: 'dashed',
+        marginLeft: 21
     },
     showImg: {
         height: 128,
