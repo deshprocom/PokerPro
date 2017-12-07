9bad49a977b324ce7b912dce7f55bd24ebc59ed7
Millie <millie@deshpro.com>
2017-12-07 18:08:27 +0800

    样式修改

--- src/pages/malls/order/LeaveMessage.js
+++ src/pages/malls/order/LeaveMessage.js
@@ -1,5 +1,5 @@
 import React, {PureComponent, PropTypes} from 'react';
-import {View, StyleSheet, TextInput, Keyboard} from 'react-native';
+import {View, StyleSheet, TextInput, Keyboard,Text} from 'react-native';
 import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../../../Themes';
 import I18n from 'react-native-i18n';
 
@@ -20,36 +20,50 @@ export default class LeaveMessage extends PureComponent {
 
     render() {
         return (
-            <TextInput
-                ref={ref => this.remark = ref}
-                style={styleL.messageView}
-                placeholder={I18n.t('leaveMessage')}
-                placeholderTextColor="#AAAAAA"
-                multiline={true}
-                textAlignVertical='top'
-                returnKeyType={'done'}
-                underlineColorAndroid="transparent"
-                onChangeText={(text) => {
-                    this.setState({text});
-                }}
-                onSubmitEditing={() => {
-                    Keyboard.dismiss();
-                }}
-            />
+            <View style={styleL.page}>
+                <Text style={styleL.txt}>{I18n.t('leaveMessage1')}</Text>
+                <TextInput
+                    numberOfLines={1}
+                    maxLength={45}
+                    onChangeText={(text) => {
+                        this.setState({text});
+                    }}
+                    ref={ref => this.remark = ref}
+                    style={styleL.messageView}
+                    placeholder={I18n.t('leaveMessage')}
+                    placeholderTextColor="#AAAAAA"
+                    multiline={false}
+                    textAlignVertical='top'
+                    returnKeyType={'done'}
+                    underlineColorAndroid="transparent"
+                    onSubmitEditing={() => {
+                      Keyboard.dismiss();
+                    }}/>
+
+            </View>
 
         )
     }
 }
 const styleL = StyleSheet.create({
-    messageView: {
-        height: 85,
-        backgroundColor: "#FFFFFF",
+    page:{
+        height: 48,
+        backgroundColor: '#FFFFFF',
         marginTop: 11,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    txt:{
+        fontSize:14,
+        color:'#000000',
+        marginLeft:17
+    },
+    messageView: {
         fontSize: 14,
-        textAlign: 'left',
-        paddingLeft: 17,
-        paddingTop: 12,
-        paddingBottom: 50,
-        paddingRight: 17
+        color: '#000000',
+        height: 40,
+        flex: 1,
+        marginTop: 5,
+
     }
 })
