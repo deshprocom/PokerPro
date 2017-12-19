6b1adb0fcfae954947873d37775ed4ee531fbb59
Millie <millie@deshpro.com>
2017-12-19 12:29:45 +0800

    新增个人中心动态功能

--- src/pages/comment/PersonDynamicPage.js
+++ src/pages/comment/PersonDynamicPage.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, ScrollView, Text, Image, TouchableOpacity, TextInput, Modal} from 'react-native';
+import {View, StyleSheet, ScrollView, Text, Image, TouchableOpacity, TextInput, Modal,Platform} from 'react-native';
 import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../../Themes';
 import I18n from 'react-native-i18n';
 import propTypes from 'prop-types';
 import {Badge} from '../../components';
 import {util} from '../../utils/ComonHelper';
+import {NavigationBar} from '../../components';
 
 export default class PersonDynamicPage extends Component {
 
@@ -12,10 +13,29 @@ export default class PersonDynamicPage extends Component {
 
     };
 
+    personTop=()=>{
+        return(
+            <View style={styles.topPage}>
+                <Image style={styles.TopImg} source={Images.icon_deshpro}/>
+            </View>
+        )
+    };
 
     render() {
         return (
-            <View></View>
+            <View style={ApplicationStyles.bgContainer}>
+                <NavigationBar
+                    barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+                    toolbarStyle={{backgroundColor: Colors.white}}
+                    title={I18n.t('person_dynamic')}
+                    titleStyle={{color: Colors._161}}
+                    leftBtnIcon={Images.mall_return}
+                    rightBtnIcon={Images.commentWhite}
+                    leftImageStyle={{height: 19, width: 11, marginLeft: 20, marginRight: 20}}
+                    rightImageStyle={{height: 20, width: 22, marginLeft: 20, marginRight: 20}}
+                    leftBtnPress={() => router.pop()}/>
+                {this.personTop()}
+            </View>
         );
     }
 
@@ -24,6 +44,13 @@ export default class PersonDynamicPage extends Component {
 
 const styles = StyleSheet.create({
 
+    topPage:{
+        width:Metrics.screenWidth,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    TopImg:{
 
+    }
 
 });
