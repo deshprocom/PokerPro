654ef9cfb6e34e136b5902211a4e8ca5eb830e7a
Millie <millie@deshpro.com>
2017-12-22 12:15:44 +0800

    新增删除评论页面

---
+++ src/pages/comment/DeletePage.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, ScrollView, Text, Image, TouchableOpacity, TextInput, Modal, Platform} from 'react-native';
+import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../../Themes';
+import I18n from 'react-native-i18n';
+import propTypes from 'prop-types';
+import {Badge} from '../../components';
+import {util} from '../../utils/ComonHelper';
+import {NavigationBar, BaseComponent} from '../../components';
+import UltimateFlatList from '../../components/ultimate';
+import {getPersonDynamics} from '../../services/CommentDao';
+import {getDateDiff, isEmptyObject} from '../../utils/ComonHelper';
+
+export default class DeletePage extends Component {
+    state = {
+        dynamics: {}
+    };
+
+    componentDidMount() {
+
+    };
+
+
+    render() {
+        return (
+            <View style={styles.page}>
+                <NavigationBar
+                    barStyle={'dark-content'}
+                    toolbarStyle={{backgroundColor: 'white'}}
+                    leftBtnIcon={Images.mall_return}
+                    leftImageStyle={{height: 19, width: 11, marginLeft: 17, marginRight: 20}}
+                    leftBtnPress={() => router.pop()}/>
+
+                <Text style={styles.Txt1}>
+                    {I18n.t('your')}{I18n.t('de')}{I18n.t('deleted')}
+                </Text>
+            </View>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+
+    page:{
+        flex:1,
+        flexDirection:'column',
+        alignItems:'center',
+
+    },
+    Txt1:{
+        fontSize: 20,
+        color: '#CCCCCC',
+        marginTop:120
+    },
+    Txt2: {
+        fontSize: 15,
+        color: '#CCCCCC',
+        marginTop:8
+    }
+
+});
