1dabb57c1a2fc37a09053f0085448355ae01264a
Millie <millie@deshpro.com>
2017-12-25 10:39:13 +0800

    我收到的回复

--- src/pages/comment/ReceivedReplyPage.js
+++ src/pages/comment/ReceivedReplyPage.js
@@ -102,7 +102,7 @@ export default class ReceivedReplyPage extends Component {
                     </View>
                     <View style={styles.replyView}>
                         <Text style={styles.replyTxt1}>
-                            {mine.typological_type === "reply" ? I18n.t('replied') : I18n.t('liked')}
+                            {item.type === "reply" ? I18n.t('replied') : I18n.t('liked')}
                         </Text>
                         <Text style={styles.replyTxt2}>
                             {I18n.t('your_comment')}：
