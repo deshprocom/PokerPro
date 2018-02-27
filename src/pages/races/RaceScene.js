944b790477e75328f835860ead709e863a128843
Millie <millie@deshpro.com>
2018-02-27 15:07:07 +0800

    换掉赛事分享按钮 评论分享按钮

--- src/pages/races/RaceScene.js
+++ src/pages/races/RaceScene.js
@@ -186,7 +186,7 @@ class RaceScene extends Component {
                         this.props.params.race_id)
                 }}>
                 <Image style={styles.imgShare}
-                       source={Images.share}/>
+                       source={Images.share2}/>
             </TouchableOpacity>
 
 
