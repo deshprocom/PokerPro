36c021fe25f6762878363c9e8e822bcb751864e4
lorne <2270333671@qq.com>
2018-04-03 17:44:06 +0800

    长帖详情

--- src/pages/socials/LongArticle.js
+++ src/pages/socials/LongArticle.js
@@ -6,27 +6,76 @@ import {
     TouchableOpacity,
     Platform,
     ScrollView,
-    Dimensions
+    StyleSheet
 } from 'react-native';
 import {reallySize} from "./Header";
 import {ImageLoad, NavigationBar} from '../../components'
 import I18n from "react-native-i18n";
-import {Colors, Images} from '../../Themes';
-import {styles} from './MomentList';
+import {Colors, Images, Metrics} from '../../Themes';
 import HTML from 'react-native-render-html';
-import {topics_like} from "../../services/SocialDao";
-import {agoDynamicDate} from "../../utils/ComonHelper";
+import {topics_like, topics_details} from "../../services/SocialDao";
+import {getDateDiff} from "../../utils/ComonHelper";
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 17,
+        color: Colors.txt_444,
+        fontWeight: 'bold',
+        paddingTop: 12,
+        paddingBottom: 12
+    },
+    avatar: {
+        height: 44,
+        width: 44,
+        borderRadius: 22
+    },
+    nick_name: {
+        fontSize: 15,
+        color: Colors._666
+    },
+    time: {
+        fontSize: 12,
+        color: Colors._AAA
+    },
+    focus: {
+        fontSize: 14,
+        color: Colors.txt_444,
+        paddingRight: 20,
+        paddingLeft: 20,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 2,
+        borderWidth: 1,
+        borderColor: Colors.txt_444
+    },
+    info: {
+        width: '100%'
+    },
+    btn_like: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    like: {
+        height: reallySize(15),
+        width: reallySize(15)
+    },
+})
 
 export default class LongArticle extends PureComponent {
 
+    componentDidMount() {
+        const {id} = this.props.params.article;
+        topics_details(id)
+    }
+
     render() {
 
-        const {user, created_at, likes, comments, id, body_type,body} = this.props.params.article;
+        const {user, created_at, likes, comments, id, body_type, body, title, page_views} = this.props.params.article;
 
         return <View style={{flex: 1, backgroundColor: 'white'}}>
             <NavigationBar
                 barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
-                toolbarStyle={{backgroundColor: Colors.white}}
+                toolbarStyle={{backgroundColor: Colors.white, borderBottomWidth: 1, borderBottomColor: Colors._ECE}}
                 title={'详情'}
                 titleStyle={{color: Colors._333}}
                 leftBtnIcon={Images.ic_back}
@@ -34,27 +83,45 @@ export default class LongArticle extends PureComponent {
                 leftBtnPress={() => router.pop()}
                 rightBtnIcon={Images.social.more_3}
                 rightImageStyle={{height: 4, width: 20, marginLeft: 20, marginRight: 20}}/>
-            <ScrollView>
-                <View style={styles.user}>
-                    <ImageLoad
-                        style={styles.avatar}
-                        source={{uri: user.avatar}}/>
+            <View style={{marginRight: 17, marginLeft: 17}}>
+                <View style={styles.info}>
+                    <Text style={styles.title}>{title}</Text>
+                    <View style={styles.btn_like}>
+                        <ImageLoad style={styles.avatar}
+                                   source={{uri: user.avatar}}/>
 
-                    <Text style={styles.nick_name}>{user.nick_name}</Text>
-                    <View style={{flex: 1}}/>
+                        <View style={{marginLeft: 12}}>
+                            <Text style={styles.nick_name}>{user.nick_name}</Text>
+                            <Text style={[styles.time, {marginTop: 5}]}>{getDateDiff(created_at)}·深圳</Text>
+                        </View>
 
-                    {body_type === 'long' ? <Text style={styles.txt_long}>长帖</Text> : null}
+                        <View style={{flex: 1}}/>
+
+                        <Text style={styles.focus}>关注</Text>
+
+                    </View>
 
                 </View>
 
-                <HTML html={body}
-                      tagsStyles={{p: {marginLeft: 17, marginRight: 17}}}
-                      imagesMaxWidth={Dimensions.get('window').width}/>
-                {/*帖子时间、地点*/}
-                <View style={styles.bottom}>
-                    <Text style={styles.time}>{agoDynamicDate(created_at)}·深圳</Text>
 
+                <HTML
+                    imagesMaxWidth={Metrics.screenWidth - 34}
+                    html={body}
+                    tagsStyles={{
+                        p: {
+                            color: Colors.txt_444,
+                            fontSize: 15,
+                            lineHeight: 25,
+                            marginTop: 14,
+                            marginBottom: 14
+                        }
+                    }}/>
+
+                <View style={styles.btn_like}>
                     <View style={{flex: 1}}/>
+
+                    <Text style={styles.time}>阅读</Text>
+                    <Text style={[styles.time, {marginLeft: 4, marginRight: 20}]}>{page_views}</Text>
                     <TouchableOpacity
                         onPress={() => {
                             topics_like(id, data => {
@@ -69,18 +136,12 @@ export default class LongArticle extends PureComponent {
                         <Image
                             style={styles.like}
                             source={Images.social.like_gray}/>
-                        <Text style={[styles.time, {marginLeft: 4, marginRight: 25}]}>{likes}</Text>
+                        <Text style={[styles.time, {marginLeft: 4}]}>{likes}</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity
-                        style={styles.btn_like}>
-                        <Image
-                            style={styles.like}
-                            source={Images.social.comment_gray}/>
-                        <Text style={[styles.time, {marginLeft: 4}]}>{comments}</Text>
-                    </TouchableOpacity>
                 </View>
-            </ScrollView>
+
+            </View>
 
 
         </View>
