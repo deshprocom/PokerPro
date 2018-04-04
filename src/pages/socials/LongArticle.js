d833c2854b25093a7c8b1133835ed4e449096f21
lorne <2270333671@qq.com>
2018-04-04 16:11:49 +0800

    点赞

--- src/pages/socials/LongArticle.js
+++ src/pages/socials/LongArticle.js
@@ -110,7 +110,8 @@ const styles = StyleSheet.create({
 export default class LongArticle extends PureComponent {
 
     state = {
-        comments_count: 0
+        comments_count: 0,
+        article: this.props.params.article
     }
 
     componentDidMount() {
@@ -119,7 +120,7 @@ export default class LongArticle extends PureComponent {
     }
 
     render() {
-        const {id} = this.props.params.article;
+        const {id} = this.state.article;
 
         return <View style={{flex: 1, backgroundColor: 'white'}}>
             <NavigationBar
@@ -171,74 +172,76 @@ export default class LongArticle extends PureComponent {
                     }}
                     like={() => {
 
+                        topics_like(id, data => {
+                            let article = {...this.state.article};
+                            article.likes = data.total_likes;
+                            this.setState({
+                                article
+                            })
+
+                        }, err => {
+                            console.log(err)
+                        })
+
                     }}
-                />
-            </View>
-
-
-        </View>
-    }
-
-    flatHeader = () => {
-
-
-        const {user, created_at, likes, comments, id, body_type, body, title, page_views} = this.props.params.article;
-        return <View>
-            <View style={{paddingLeft: 17, paddingRight: 17, backgroundColor: 'white'}}>
-                <View style={styles.info}>
-                    <Text style={styles.title}>{title}</Text>
-                    <View style={styles.btn_like}>
-                        <ImageLoad style={styles.avatar}
-                                   source={{uri: user.avatar}}/>
-
-                        <View style={{marginLeft: 12}}>
-                            <Text style={styles.nick_name}>{user.nick_name}</Text>
-                            <Text style={[styles.time, {marginTop: 5}]}>{getDateDiff(created_at)}·深圳</Text>
+                        />
                         </View>
 
-                        <View style={{flex: 1}}/>
 
-                        <Text style={styles.focus}>关注</Text>
+                        </View>
+                        }
 
-                    </View>
+                    flatHeader=() => {
+
+
+                const {user, created_at, likes, comments, id, body_type, body, title, page_views} = this.state.article;
+                return <View>
+                <View style={{paddingLeft: 17, paddingRight: 17, backgroundColor: 'white'}}>
+                <View style={styles.info}>
+                <Text style={styles.title}>{title}</Text>
+                <View style={styles.btn_like}>
+                <ImageLoad style={styles.avatar}
+                source={{uri: user.avatar}}/>
+
+                <View style={{marginLeft: 12}}>
+                <Text style={styles.nick_name}>{user.nick_name}</Text>
+                <Text style={[styles.time, {marginTop: 5}]}>{getDateDiff(created_at)}·深圳</Text>
+                </View>
+
+                <View style={{flex: 1}}/>
+
+                <Text style={styles.focus}>关注</Text>
+
+                </View>
 
                 </View>
 
 
                 <HTML
-                    imagesMaxWidth={Metrics.screenWidth - 34}
-                    html={body}
-                    tagsStyles={{
-                        p: {
-                            color: Colors.txt_444,
-                            fontSize: 15,
-                            lineHeight: 25,
-                            marginTop: 14,
-                            marginBottom: 14
-                        }
-                    }}/>
+                imagesMaxWidth={Metrics.screenWidth - 34}
+                html={body}
+                tagsStyles={{
+                    p: {
+                        color: Colors.txt_444,
+                        fontSize: 15,
+                        lineHeight: 25,
+                        marginTop: 14,
+                        marginBottom: 14
+                    }
+                }}/>
 
                 <View style={[styles.btn_like, {marginTop: 15}]}>
-                    <View style={{flex: 1}}/>
+                <View style={{flex: 1}}/>
 
-                    <Text style={styles.time}>阅读</Text>
-                    <Text style={[styles.time, {marginLeft: 4, marginRight: 20}]}>{page_views}</Text>
-                    <TouchableOpacity
-                        onPress={() => {
-                            topics_like(id, data => {
-                                item.likes = data.total_likes;
-                                this.listView && this.listView.updateDataSource(this.listView.getRows())
-
-                            }, err => {
-                                console.log(err)
-                            })
-                        }}
-                        style={styles.btn_like}>
-                        <Image
-                            style={styles.like}
-                            source={Images.social.like_gray}/>
-                        <Text style={[styles.time, {marginLeft: 4}]}>{likes}</Text>
-                    </TouchableOpacity>
+                <Text style={styles.time}>阅读</Text>
+                <Text style={[styles.time, {marginLeft: 4, marginRight: 20}]}>{page_views}</Text>
+                <View
+                style={styles.btn_like}>
+                <Image
+                style={styles.like}
+                source={Images.social.like_gray}/>
+                <Text style={[styles.time, {marginLeft: 4}]}>{likes}</Text>
+                </View>
 
                 </View>
 
@@ -247,42 +250,42 @@ export default class LongArticle extends PureComponent {
                     borderTopWidth: 1, borderTopColor: Colors._ECE,
                     marginTop: 10
                 }]}>
-                    <Text style={styles.comment}>{`全部评论 (${this.state.comments_count})`}</Text>
+                <Text style={styles.comment}>{`全部评论 (${this.state.comments_count})`}</Text>
                 </View>
 
-            </View>
-            <View style={{height: 1, backgroundColor: Colors._ECE}}/>
-        </View>
-    }
+                </View>
+                <View style={{height: 1, backgroundColor: Colors._ECE}}/>
+                </View>
+            }
 
-    onFetch = (page = 1, startFetch, abortFetch) => {
-        const {id} = this.props.params.article;
-        topics_comments(id, data => {
-            startFetch(data.items, 15)
-            this.setState({
+                  onFetch=(page = 1, startFetch, abortFetch) => {
+                const {id} = this.props.params.article;
+                topics_comments(id, data => {
+                startFetch(data.items, 15)
+                this.setState({
                 comments_count: data.comments
             })
-        }, err => {
-            abortFetch()
-        }, {page, page_size: 20})
-    }
+            }, err => {
+                abortFetch()
+            }, {page, page_size: 20})
+            }
 
-    itemView = (item) => {
-        const {
-            avatar, nick_name, created_at, official,
-            recommended, body
-        } = item;
-        return <View style={{
-            width: '100%', paddingLeft: 17, paddingRight: 17,
-            paddingTop: 12
-        }}>
-            <View style={styles.btn_like}>
+                  itemView=(item) => {
+                const {
+                avatar, nick_name, created_at, official,
+                recommended, body
+            } = item;
+                return <View style={{
+                    width: '100%', paddingLeft: 17, paddingRight: 17,
+                    paddingTop: 12
+                }}>
+                <View style={styles.btn_like}>
                 <ImageLoad style={styles.c_avatar}
-                           source={{uri: avatar}}/>
+                source={{uri: avatar}}/>
 
                 <View style={{marginLeft: 10}}>
-                    <Text style={styles.c_nick}>{nick_name}</Text>
-                    <Text style={[styles.c_time, {marginTop: 5}]}>{getDateDiff(created_at)}</Text>
+                <Text style={styles.c_nick}>{nick_name}</Text>
+                <Text style={[styles.c_time, {marginTop: 5}]}>{getDateDiff(created_at)}</Text>
                 </View>
 
                 {official ? <Text style={[styles.c_tag, {
@@ -298,29 +301,29 @@ export default class LongArticle extends PureComponent {
                 <View style={{flex: 1}}/>
 
                 <Image style={styles.c_comment}
-                       source={Images.social.reply}/>
+                source={Images.social.reply}/>
 
 
-            </View>
+                </View>
 
-            <Text style={styles.c_body}>{body}</Text>
+                <Text style={styles.c_body}>{body}</Text>
 
-            <TouchableOpacity style={{
-                height: 20,
-                backgroundColor: '#ECECEE',
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginLeft: 54,
-                marginTop: 8
-            }}>
+                <TouchableOpacity style={{
+                    height: 20,
+                    backgroundColor: '#ECECEE',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginLeft: 54,
+                    marginTop: 8
+                }}>
                 <Text style={[styles.c_nick, {marginLeft: 6}]}>查看34条回复></Text>
 
-            </TouchableOpacity>
+                </TouchableOpacity>
 
-            <View style={{height: 1, backgroundColor: Colors._ECE, marginTop: 8}}/>
+                <View style={{height: 1, backgroundColor: Colors._ECE, marginTop: 8}}/>
 
 
-        </View>
+                </View>
 
-    }
-}
+            }
+                }
