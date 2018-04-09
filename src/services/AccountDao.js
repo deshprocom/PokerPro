7b9fc9bde374cd9449659c2d36035b8254c1eea5
Crazy <1049260505@qq.com>
2018-04-09 11:30:16 +0800

    未下载视频点击下载播放

--- src/services/AccountDao.js
+++ src/services/AccountDao.js
@@ -252,7 +252,6 @@ function getJmessageInfo(avatar) {
             },
             //登录成功回调
             () => {
-                jmsgUserName = username;
                 //下载网络图片
                 // let rootPath = fs.DocumentDirectoryPath;
                 // let savePath = rootPath + '/temp_icon.jpg';
