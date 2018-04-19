47d7f507d34c4c3eb7d0254b48741087a34a661b
lorne <2270333671@qq.com>
2018-04-19 15:13:31 +0800

    附近的人

--- src/pages/socials/NearFriend.js
+++ src/pages/socials/NearFriend.js
@@ -10,16 +10,18 @@ import {
 } from 'react-native';
 import I18n from "react-native-i18n";
 import {Colors, Images, ApplicationStyles, Metrics} from "../../Themes";
-import {NavigationBar} from '../../components';
+import {NavigationBar, ImageLoad} from '../../components';
 import ErrLocation from '../comm/ErrLocation';
 import {getNearBys, postNearBys} from '../../services/SocialDao'
-import {getDateDiff} from '../../utils/ComonHelper'
+import {strNotNull} from '../../utils/ComonHelper'
 
 const styles = StyleSheet.create({
     avatar: {
         height: 54,
         width: 54,
-        borderRadius: 54
+        borderRadius: 27,
+        marginLeft: 17,
+        marginRight: 20,
     },
     name: {
         fontSize: 15,
@@ -29,9 +31,13 @@ const styles = StyleSheet.create({
         fontSize: 12,
         color: Colors._AAA
     },
-    gender: {
+    male: {
         height: 9,
         width: 9
+    },
+    woman: {
+        height: 10,
+        width: 8
     }
 })
 
@@ -39,12 +45,19 @@ export default class NearFriend extends PureComponent {
 
     state = {
         geolocation: true,
-        nearby_users: [],
+        nearby_users: [{
+            avatar: "https://cdn-upyun.deshpro.com/faces/e2c/807/e2c8071236d3c5b913d6f98872d0112c.jpg",
+            distance: 8.757719710805624,
+            gender: 2,
+            nick_name: "Ricky",
+            signature: "这个人很懒，什么都没写。",
+            user_id: "014b58eac7cf2955a78b64393872fbda"
+        }],
         refreshing: false
 
     }
 
-    componentDidMount() {
+    componentWillMount() {
         navigator.geolocation.getCurrentPosition(data => {
             console.log('位置坐标：', data)
             if (Platform.OS === 'ios') {
@@ -108,33 +121,57 @@ export default class NearFriend extends PureComponent {
 
     renderItem = ({item}) => {
         const {nick_name, gender, avatar, signature, distance} = item;
-        let sex = gender === 0 ? Images.social.male : gender === 1 ? Images.woman : "";
+        let sex = gender === 0 ? Images.social.male : gender === 1 ? Images.social.woman : "";
 
-        return <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Image style={styles.avatar}
-                   defualtSource={Images.home_avatar}
-                   source={{uri: avatar}}/>
+        return <TouchableOpacity
+            onPress={() => {
+                global.router.toUserTopicPage(item)
+            }}
+            style={{backgroundColor: 'white'}}>
+            <View style={{
+                flexDirection: 'row', alignItems: 'center',
+                height: 75
+            }}>
+                <ImageLoad style={styles.avatar}
+                           emptyBg={Images.home_avatar}
+                           source={{uri: avatar}}/>
 
-            <View>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Text>{nick_name}</Text>
-                    <Image
-                        source={sex}
-                        style={styles.gender}/>
+                <View>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Text>{nick_name}</Text>
+                        {gender === 2 ? null : <Image
+                            source={sex}
+                            style={gender === 0 ? styles.male : styles.woman}/>}
+
+
+                    </View>
+
+                    <Text>{this.covertTom(distance)}</Text>
                 </View>
 
-                <Text>{`${distance} | 时间`}</Text>
+                <View style={{flex: 1}}/>
+
+
+                {strNotNull(signature) ? <Text style={{
+                    fontSize: 12, color: Colors._888,
+                    backgroundColor: Colors._ECE, padding: 5,
+                    borderRadius: 5,
+                    maxHeight: 54,
+                    maxWidth: 110,
+                    marginRight: 17
+                }}>{signature}</Text> : null}
+
+
             </View>
+            <View style={{marginLeft: 91, height: 1, backgroundColor: Colors._ECE}}/>
+        </TouchableOpacity>
+    }
 
-            <Text style={{
-                fontSize: 12, color: Colors._888,
-                backgroundColor: Colors._ECE, padding: 5,
-                borderRadius: 5,
-                maxHeight: 54,
-                maxWidth: 110,
-                marginRight: 17
-            }}>{signature}</Text>
+    covertTom = (distance) => {
+        if (distance < 1) {
+            return Number.parseInt(distance * 1000) + "m"
+        }
 
-        </View>
+        return Number.parseInt(distance) + "km"
     }
 }
